51e338bcdc845b3750da6c19f2f7dcacc8f1d34a
Akarshana06 <akarshana027@gmail.com>
2025-08-29 14:28:23 +0530

    frontend initiated

---
+++ frontend/app/(tabs)/FaceEnrollScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function FaceEnrollScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
+      <Text style={{ color: "black" }}>Face Enroll Screen</Text>
+    </View>
+  );
+}
+
